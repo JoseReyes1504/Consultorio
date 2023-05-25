8198718427d4c29e49e8914923e4c1be815f7fa6
JoseReyes1504 <eduardo.reyessuazo@gmail.com>
2023-05-24 23:08:43 -0600

    cambios

--- CML/CML/Program.cs
+++ CML/CML/Program.cs
@@ -16,7 +16,7 @@ namespace CML
         {
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
-            Application.Run(new FrmConsulta());
+            Application.Run(new FrmMenu());
         }
     }
 }
